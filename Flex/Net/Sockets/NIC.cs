893f859b7dee11a9723a8faefe723a19ae3c0c07
ohikkoshi <ohikkoshi@icloud.com>
2022-07-15 14:08:10 +0900

    Update NIC.cs

--- Flex/Net/Sockets/NIC.cs
+++ Flex/Net/Sockets/NIC.cs
@@ -1,59 +1,98 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 
 namespace Flex.Net.Sockets
 {
 	public class NIC
 	{
-		public static string IPv4()
+		static List<NetworkInterface> adapters = new();
+		static List<NetworkInterface> Adapters
 		{
-			try {
-				var hostName = Dns.GetHostName();
-				var addresses = Dns.GetHostAddresses(hostName);
+			get {
+#if (UNITY_EDITOR || UNITY_STANDALONE)
+				if (adapters.Count == 0) {
+					var query = NetworkInterface.GetAllNetworkInterfaces();
 
-				foreach (var address in addresses) {
-					if (address.AddressFamily == AddressFamily.InterNetwork) {
-						return address.ToString();
-					}
-				}
-			} catch (System.Exception) {
-				throw;
-			}
+					foreach (var nic in query) {
+						if (nic.OperationalStatus != OperationalStatus.Up) {
+							continue;
+						}
 
-			return null;
-		}
+						if (nic.NetworkInterfaceType == NetworkInterfaceType.Loopback) {
+							continue;
+						}
 
-		public static string IPv4(NetworkInterfaceType networkInterfaceType)
-		{
-			try {
-				var array = NetworkInterface.GetAllNetworkInterfaces();
-				const string LocalLoopbackAddress = "127.0";
-				const string LinkLocalAddress = "169.254.";
+						//var type = nic.NetworkInterfaceType.ToString();
+						var name = nic.Name.ToLower();
+						var desc = nic.Description.ToLower();
+						var key = $"{name}|{desc}";
 
-				foreach (var nic in array) {
-					if (nic.NetworkInterfaceType == networkInterfaceType) {
-						var props = nic.GetIPProperties();
-
-						foreach (var ip in props.UnicastAddresses) {
-							if (ip.Address.AddressFamily == AddressFamily.InterNetwork) {
-								var addr = ip.Address.ToString();
-
-								if (string.IsNullOrEmpty(addr) ||
-									addr.StartsWith(LocalLoopbackAddress) ||
-									addr.StartsWith(LinkLocalAddress)) {
-									return null;
-								}
-
-								return addr;
-							}
+						if (
+#if (UNITY_EDITOR || UNITY_STANDALONE)
+							key.IndexOf("wi-fi direct virtual adapter") < 0 &&
+							(
+							// Windows
+							key.IndexOf("wifi") >= 0 ||
+							key.IndexOf("wi-fi") >= 0 ||
+							key.IndexOf("wireless") >= 0 ||
+							// macOS
+							key.IndexOf("en1") >= 0
+							)
+#elif UNITY_IOS
+							// iOS
+							key.IndexOf("en0") >= 0 ||
+#elif UNITY_ANDROID
+							// Android
+							key.IndexOf("wlan") >= 0 ||
+#endif
+						) {
+							adapters.Add(nic);
 						}
 					}
 				}
-			} catch (System.Exception) {
-				throw;
+#endif
+				return adapters;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string IPv4()
+		{
+			// Wi-Fi
+			foreach (var nic in Adapters) {
+				var props = nic.GetIPProperties().UnicastAddresses;
+
+				foreach (var prop in props) {
+					if (prop.Address.AddressFamily == AddressFamily.InterNetwork) {
+						return prop.Address.ToString();
+					}
+				}
+			}
+
+			// LAN
+			return Ethernet();
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		static string Ethernet()
+		{
+			foreach (var nic in NetworkInterface.GetAllNetworkInterfaces()) {
+				if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && nic.OperationalStatus == OperationalStatus.Up) {
+					foreach (var unicast in nic.GetIPProperties().UnicastAddresses) {
+						if (unicast.Address.AddressFamily == AddressFamily.InterNetwork) {
+							return unicast.Address.ToString();
+						}
+					}
+				}
 			}
 
 			return null;
